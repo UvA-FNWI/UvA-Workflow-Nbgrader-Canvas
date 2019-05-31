4a419a56f60433f9ff338cc979b5182f97387fc9
jaspervdh96 <jaspervdh96@hotmail.com>
2019-05-31 22:01:53 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='Workflow',  
+
+     version='0.1',
+
+     scripts=['workflow'] ,
+
+     author="Jasper van der Heide",
+
+     author_email="jaspervdh96@hotmail.com",
+
+     description="A workflow for Canvas and Nbgrader",
+
+     long_description=long_description,
+
+   long_description_content_type="text/markdown",
+
+     url="https://github.com/jaspervdh96/Workflow",
+
+     packages=setuptools.find_packages(),
+
+     classifiers=[
+
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent"
+
+     ],
+
+ )
